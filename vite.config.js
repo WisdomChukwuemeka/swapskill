5a52de2e62d4ccc24a4d1d0f55ee19571cfd9a17
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-08-05 01:20:05 +0100

    css

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+      headers: {
+        'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-eval';",
+    },
+  },
 })
